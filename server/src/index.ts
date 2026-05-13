01b7ea70f1e1f0d6497416657bd895d8b28a306c
margdarsak <152509759+margdarsak@users.noreply.github.com>
2026-05-13 23:04:12 +0530

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ import path from 'path';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.use(cors());
 app.use(express.json());
